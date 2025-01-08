26c5076d92a5b518940875cabc3669cc17ca8cba
franz1us <151651409+franz1us@users.noreply.github.com>
2025-01-08 15:16:45 +0700

    IconSymbol: Set iconSymbol

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,32 +13,31 @@ const MAPPING = {
   'statistic.chart': 'bar-chart',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
-} as Partial<
-  Record<
-    import('expo-symbols').SymbolViewProps['name'],
-    React.ComponentProps<typeof MaterialIcons>['name']
-  >
->;
+} as const;
 
 
-export type IconSymbolName = keyof typeof MAPPING;
 
-/**
- * An icon component that uses native SFSymbols on iOS, and MaterialIcons on Android and web. This ensures a consistent look across platforms, and optimal resource usage.
- *
- * Icon `name`s are based on SFSymbols and require manual mapping to MaterialIcons.
- */
-export function IconSymbol({
-  name,
-  size = 24,
-  color,
-  style,
-}: {
-  name: IconSymbolName;
-  size?: number;
-  color: string | OpaqueColorValue;
-  style?: StyleProp<ViewStyle>;
-  weight?: SymbolWeight;
-}) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]}  />;
-}
+
+export type IconSymbolName =
+  | 'house.fill'
+  | 'paperplane.fill'
+  | 'ranking.star'
+  | 'statistic.chart'
+  | 'chevron.left.forwardslash.chevron.right'
+  | 'chevron.right';
+
+  export function IconSymbol({
+    name,
+    size = 24,
+    color,
+    style,
+  }: {
+    name: IconSymbolName; // Ensure it's explicitly typed
+    size?: number;
+    color: string | OpaqueColorValue;
+    style?: StyleProp<ViewStyle>;
+    weight?: SymbolWeight;
+  }) {
+    return <MaterialIcons color={color} size={size} name={MAPPING[name]} />;
+  }
+  
