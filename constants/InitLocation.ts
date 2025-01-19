f6ab4b0bd236d614e78d8ce59533adafb4efee6e
franz1us <151651409+franz1us@users.noreply.github.com>
2025-01-19 20:42:50 +0700

    Constants: Create Mockup data of Location

---
+++ constants/InitLocation.ts
@@ -0,0 +1,16 @@
+import { TypeLocation } from "@/hooks/useLocations";
+
+
+export const InitLocation = [
+    { id: "1", name: "Chiang Rai City", pm25: 42, favorite: false },
+    { id: "2", name: "Mae Sai", pm25: 55, favorite: false },
+    { id: "3", name: "Wiang Pa Pao", pm25: 37, favorite: false },
+    { id: "4", name: "Chiang Khong", pm25: 48, favorite: false },
+    { id: "5", name: "Mae Chan", pm25: 50, favorite: false },
+    { id: "6", name: "Phan District", pm25: 39, favorite: false },
+    { id: "7", name: "Mae Fah Luang", pm25: 60, favorite: false },
+    { id: "8", name: "Thoen District", pm25: 45, favorite: false },
+    { id: "9", name: "Chiang Saen", pm25: 53, favorite: false },
+    { id: "10", name: "Doi Luang", pm25: 35, favorite: false },
+  ];
+  
