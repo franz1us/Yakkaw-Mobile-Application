6f3310d990139a38ed86d7f5bceda7ecdf8f0c07
MABiuS1 <66194783+MABiuS1@users.noreply.github.com>
2025-01-19 23:31:33 +0700

    Create: Notification pages

---
+++ app/Notification.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { ThemedView } from '@/components/ThemedView'
+
+const Notification = () => {
+  return (
+    <ThemedView><Text>Notification</Text></ThemedView>
+  )
+}
+
+export default Notification
+
+const styles = StyleSheet.create({})
