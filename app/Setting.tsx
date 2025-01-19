2b5e0053c389e8634982971eac4e96c72dca118d
MABiuS1 <66194783+MABiuS1@users.noreply.github.com>
2025-01-19 21:20:03 +0700

    Update: layout style and create setting page

---
+++ app/Setting.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { ThemedView } from '@/components/ThemedView'
+
+const Settings = () => {
+  return (
+    <ThemedView><Text>Setting</Text></ThemedView>
+  )
+}
+
+export default Settings
+
+const styles = StyleSheet.create({})
