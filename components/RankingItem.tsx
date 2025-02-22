d73783f21c8cdf7d939bfe274183c1d06f6470e2
franz1us <151651409+franz1us@users.noreply.github.com>
2025-02-22 16:27:44 +0700

    Components: Separate Condition out of RankingItem

--- components/RankingItem.tsx
+++ components/RankingItem.tsx
@@ -9,61 +9,11 @@ import {
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { Ranking_type } from "@/constants/Ranking_Data";
 import { router } from "expo-router";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { ConditionRankingStyles, Status, trendArrow,imageStatus} from "./ConditionForRanking";
 
 interface RankingItemProps {
   item: Ranking_type;
 }
-const ConditionRankingStyles = (av6h: number) => {
-  return {
-    borderColor:
-      av6h >= 91 ? "#FF8787"
-        : av6h >= 51 ? "#FFD68B"
-        : av6h >= 38 ? "#FFFFA8"
-        : av6h >= 26 ? "#BFFFA1"
-        : "#C1E5FF",
-
-    backgroundColor:
-      av6h >= 91 ? "#FF2C2C"
-        : av6h >= 51 ? "#FFA500"
-        : av6h >= 38 ? "#F0F000"
-        : av6h >= 26 ? "#38B000"
-        : "#2EA8FF",
-
-    textColor:
-      av6h >= 91 ? "white"
-        : av6h >= 51 ? "black"
-        : av6h >= 38 ? "black"
-        : "white",
-  };
-};
-
-const Status = (av6h: number) => {
-  if (av6h >= 91) return "Dangerous";
-  if (av6h >= 51) return "Unhealthy";
-  if (av6h >= 38) return "Moderate";
-  if (av6h >= 26) return "Good";
-  return "Excellent";
-};
-
-const trendArrow = (trend: string) => {
-  if (trend === "d") return <MaterialIcons name="trending-down" size={20} color="green" />;
-  if (trend === "u") return <MaterialIcons name="trending-up" size={20} color="red" />;
-  if (trend === "e") return <MaterialIcons name="trending-flat" size={20} color="gray" />;
-  return null;
-};
-
-
-
-const imageStatus = (av6h: number) => {
-  if (av6h >= 91) return require('../assets/images/Dangerous.png');
-  if (av6h >= 51) return require('../assets/images/Unhealthy.png');
-  if (av6h >= 38) return require('../assets/images/Moderate.png');
-  if (av6h >= 26) return require('../assets/images/Good.png');
-  return require('../assets/images/Excellent.png');
-};
-
-
 
 const RankingItem: React.FC<RankingItemProps> = ({ item }) => {
   const styleCondition =  ConditionRankingStyles(item.av6h);
